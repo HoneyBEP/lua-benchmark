7f45cdfe9775dbe137d79c2e35242d1f4836bb87
Nordin van Nes <nordin_v_nes@hotmail.com>
2018-04-24 14:54:23 +0200

    Update shopify benchmark

--- benchmark.lua
+++ benchmark.lua
@@ -1,1 +1,9 @@
-print('Hello')
+function benchmark(depth)
+    print('print `depth` from Lua: ' .. depth)
+    return  depth + 10
+end
+
+function countFromGo()
+    print('echo `count` in Lua')
+    count()
+end
--- implementation/shopify/shopify.go
+++ implementation/shopify/shopify.go
@@ -14,9 +14,32 @@ Shopify uses Lua 5.2
 func Run() string {
 	L := lua.NewState()
 	lua.OpenLibraries(L)
-	if err := lua.DoFile(L, "lua-tests/v5.2/all.lua"); err != nil {
+	lua.DoFile(L, "benchmark.lua")
+	if err := lua.DoFile(L, "benchmark.lua"); err != nil {
 		return fmt.Sprintf("%s \n", err)
 	}
 
+	// Lua function in Go
+	// lua.DoString(L,"print(benchmark(10))")
+	L.Global("benchmark")
+	L.PushInteger(10)
+
+	L.Call(1, 1)
+	L.ToInteger(1)
+	fmt.Printf("print `depth` from Go: %v \n",  L.ToValue(1))
+
+	// Register Go function in Lua
+	L.Register("count", func(l *lua.State) int {
+		L.PushInteger(8)
+		fmt.Println("echo `count` in Go")
+		return 1
+	})
+
+	// Run Go function in Lua
+	// L.Call(0, 1)
+	lua.DoString(L, "countFromGo()")
+
 	return ""
 }
+
+
